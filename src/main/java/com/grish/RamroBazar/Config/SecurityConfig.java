4efb83ec1570648cd0ff002a1d0caa8c3e7a3ceb
Grish Shrestha <grishshrestha2@gmail.com>
2025-03-19 20:04:12 +0545

    Started JWT Authentication

--- src/main/java/com/grish/RamroBazar/Config/SecurityConfig.java
+++ src/main/java/com/grish/RamroBazar/Config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.grish.RamroBazar.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -48,6 +50,11 @@ public class SecurityConfig {
         authProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         authProvider.setUserDetailsService(userDetailsService);
         return authProvider;
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 //    @Bean
